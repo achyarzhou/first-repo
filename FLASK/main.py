87b685e5882e40c0ad7dfe5fe3561b52bae6e574
achyarzhou <m.achyar02@gmail.com>
2025-01-18 09:27:23 +0700

    update main.py

--- FLASK/main.py
+++ FLASK/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '<h1>Project menggunakan flask sebagai framework website pribadi</h1>'
+def home(name=None):
+	return render_template('home.html')
 
+@app.route('/about')
+def about(name=None):
+	return render_template('about.html')
+
+# used to run locally
 if __name__ == '__main__':
-    app.run(debug=True)  
+	app.run(debug=True) 
+	
