afcfbbb79c1fabd1d3b72223357f32c919462032
achyarzhou <m.achyar02@gmail.com>
2026-01-15 14:29:35 +0700

    add footer.php

---
+++ mefeb/footer.php
@@ -0,0 +1,21 @@
+$ vim add.fil
+
+</div><!-- .container -->
+</main>
+
+<footer class="site-footer">
+  <div class="container footer-inner">
+    <div class="footer-left">
+      <strong>Magister Ekonomi</strong><br>
+      Fakultas Ekonomi dan Bisnis, Universitas Samudra
+    </div>
+    <div class="footer-right">
+      Jl.Meurandeh, Kota Langsa, Aceh<br>
+      © <?php echo date('Y'); ?> Universitas Samudra
+    </div>
+  </div>
+  <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
+e 
